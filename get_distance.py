45f3d19741692b8a94be7dfa68deecf582dc1512
Dmitriy <49707091+DmitriyKhodykin@users.noreply.github.com>
2019-10-31 09:30:12 +0300

    Update get_distance.py

--- get_distance.py
+++ get_distance.py
@@ -8,6 +8,7 @@ import pandas as pd
 
 token = '' # для получения - требуется зарегистрироваться на mapbox
 
+
 def get_coordinates(get_point):
     '''Возвращает координаты в формате: [Долгота{longitude}, Широта{latitude}], например, 38.05,41.06'''
     point = str(get_point)
@@ -15,7 +16,8 @@ def get_coordinates(get_point):
         f'https://api.mapbox.com/geocoding/v5/mapbox.places/{point}.json?limit=2&access_token={token}').text
     geo_point = json.loads(r)
     return str(geo_point['features'][0]['geometry']['coordinates'])[1:-1].replace(' ', '')
-    
+
+
 def distance(pointA, pointB):
     '''Возвращает расстояние в киллометрах между пунктами А и Б'''
     point1 = get_coordinates(pointA)
@@ -25,5 +27,6 @@ def distance(pointA, pointB):
         f'https://api.mapbox.com/directions/v5/mapbox/{profile}/{point1};{point2}?access_token={token}').text
     output = json.loads(r)
     return int(output['routes'][0]['distance']) // 1000
-    
-    print(distance('Москва Россия', 'Воронеж Россия'))
+
+
+print(distance('Москва Россия', 'Воронеж Россия'))
